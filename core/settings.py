45bd18d88f08eddf2957f121ca5d7bc544fcac5c
Joseph Prince <josephprince1203@gmail.com>
2025-02-07 16:23:08 -0700

    Changed to sqlite for testing

--- core/settings.py
+++ core/settings.py
@@ -151,11 +151,17 @@ DATABASES = {
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         },
-        'TEST': {
-            'NAME': 'test_Portfolio'  # Ensure test DB name is set
-        }
     }
 }
+
+
+if 'pytest' in sys.argv[0]:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',  # Use in-memory SQLite DB for tests
+    }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
