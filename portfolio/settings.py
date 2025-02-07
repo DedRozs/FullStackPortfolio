2e15aac3541a874af414b297274c6c19a98738dd
Joseph Prince <josephprince1203@gmail.com>
2025-02-06 23:20:54 -0700

    tried a different way of loading tailwind

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(CORE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(CORE_DIR, 'static'),
+    os.path.join(CORE_DIR, 'staticfiles'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
