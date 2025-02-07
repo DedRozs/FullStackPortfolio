04cc0e4f0f4d59e51e2b7be5fb4f35072efd579e
Joseph Prince <josephprince1203@gmail.com>
2025-02-07 12:17:44 -0700

    Removed whitenoise and set debug to true on prod

--- core/settings.py
+++ core/settings.py
@@ -62,7 +62,6 @@ NPM_BIN_PATH = os.getenv("NPM_BIN_PATH", "/usr/bin/npm")
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -165,9 +164,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
