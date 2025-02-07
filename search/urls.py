ac53e291d738774ccb5b86ade65b6c07d4b7dedd
Joseph Prince <josephprince1203@gmail.com>
2025-02-06 18:32:06 -0700

    Removed FAISS dependency

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import AddTextToIndexView, SearchView
+
 
 urlpatterns = [
-    path("add/", AddTextToIndexView.as_view(), name="add_to_index"),
-    path("search/", SearchView.as_view(), name="search"),
+
 ]
