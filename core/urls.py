474c2e11276fab5d738bedc88f1bdf17ab5fe0e2
Joseph Prince <josephprince1203@gmail.com>
2025-02-07 12:35:03 -0700

    Serving static files manually

--- core/urls.py
+++ core/urls.py
@@ -19,12 +19,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("apps.portfolio.urls")),  # Root URL
     
     path("__reload__/", include("django_browser_reload.urls")),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
 
 if settings.DEBUG:
