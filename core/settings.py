2bd1a111f440db5dcc0cf26d45cff03f02dc6081
Joseph Prince <josephprince1203@gmail.com>
2025-02-08 18:13:14 -0700

    Renamed actions and cleand up settings.py

--- core/settings.py
+++ core/settings.py
@@ -13,6 +13,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
+
 ALLOWED_HOSTS = ['ai-fullstack-portfolio.uc.r.appspot.com', 'localhost', 'thejosephprince.com', 'www.thejosephprince.com', 'portfolio.thejosephprince.com',]
 
 
