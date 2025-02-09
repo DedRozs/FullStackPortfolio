dcaeb1a77596de74184cc4cdd0f22f5ff92c80d6
Joseph Prince <josephprince1203@gmail.com>
2025-02-08 17:19:21 -0700

    Project reset

--- tests/auth_app_tests.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from rest_framework.test import APIClient
-from rest_framework.authtoken.models import Token
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
--- tests/portfolio_tests.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from django.contrib.auth import get_user_model
-from rest_framework.test import APIClient
-from apps.portfolio.models import Project
-
-User = get_user_model()
